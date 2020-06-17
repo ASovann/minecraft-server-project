b1f29a8f5af27d74c676103fbb8fdba2b7b1e453
ASovann <46998145+ASovann@users.noreply.github.com>
2020-06-17 11:41:58 +0200

    player list modif

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
-<head>
     <?php include './res/templates/head.php';?>
     <link rel="stylesheet" type="text/css" href="./res/css/custom.css">
     <link rel="icon" href="https://api.mcsrvstat.us/icon/lacombe92101.synology.me">
+
 </head>
 <body>
 <div class="container">
@@ -23,55 +23,56 @@
 
                 <div class="row">
 
-                <div class="col-sm-12 all">
+                    <div class="col-sm-12 all">
 
-                <div class="icon">';
-                    if ($online) {
-                        $icon = 'https://api.mcsrvstat.us/icon/' . $serverip;
-                        echo '<img src='. $icon .'>';
-                    }
+                        <div class="icon">';
+                            if ($online) {
+                                $icon = 'https://api.mcsrvstat.us/icon/' . $serverip;
+                                echo '<img src='. $icon .'>';
+                            }
 
-                echo '</div> <div class="logo">';
-                    if ($online) {
-                        $icon = 'https://api.mcsrvstat.us/icon/' . $serverip;
-                        echo '<img src='. $icon .'>';
-                    }
+                        echo '</div> 
+                        <div class="logo">';
+                            if ($online) {
+                                $icon = 'https://api.mcsrvstat.us/icon/' . $serverip;
+                                echo '<img src='. $icon .'>';
+                            }
 
-                echo '</div> <div class="info_and_status">
+                        echo '</div> 
+                        <div class="info_and_status">
+                            <h1><span id="sv_ip">lacombe92101.synology.me</span><br>
+                            <span id="status">status: ';
+                            if($online) {
+                                echo 'ONLINE';
+                            }
+                            else {
+                                echo 'OFFLINE';
+                            }
+                            echo'</span></h1>
+                            <h2 id="players_num">Players: ';
+                            if($online){
+                                echo $status -> players -> online .'/'. $status -> players -> max;
+                            }else {
+                                echo 'none';
+                            }
+                            echo '</h2>
 
-                <h1><span id="sv_ip">lacombe92101.synology.me</span><br>
-                <span id="status">status: ';
-                if($online) {
-                    echo 'ONLINE';
-                }
-                else {
-                    echo 'OFFLINE';
-                }
-                echo'</span></h1>
-                <h2 id="players_num">Players: ';
-                if($online){
-                    echo $status -> players -> online .'/'. $status -> players -> max;
-                }else {
-                    echo 'none';
-                }
-                echo '</h2>
-
-                <h3 id="version">Version: ';
-                if($online) {
-                    echo $status -> version;
-                }
-                echo '</h3>';
-                if($online){
-                    echo '<a href="./map">MAP</a>';      
-                }
-                echo '
+                            <h3 id="version">Version: ';
+                            if($online) {
+                                echo $status -> version;
+                            }
+                            echo '</h3>';
+                            if($online){
+                                echo '<a href="./map">MAP</a>';      
+                            }
+                            echo '
+                        </div>
+                    </div>
+                </div>
             </div>
-    </div>
+        '
 
-    </div>
-'
-
-?>
+    ?>
 </div>
 	<?php include './res/templates/footer.php';?>
 </body>
--- playerlist.php
+++ playerlist.php
@@ -1,10 +1,68 @@
 <html>
 <head>
+    
     <?php include './res/templates/head.php';?>
     <link rel="stylesheet" type="text/css" href="./res/css/custom.css">
     <link rel="icon" href="https://api.mcsrvstat.us/icon/lacombe92101.synology.me">
 </head>
 <body>
+<div class="container">
+	<?php
+        $serverip = 'lacombe92101.synology.me';
+        $status = json_decode(file_get_contents('https://api.mcsrvstat.us/2/' . $serverip));
+        if($status -> online == true)
+        {
+            $online = true;
 
+        }else {
+            $online = false;
+        }
+        echo'
+            <div class="black centered"></div>
+
+            <div class="row">
+
+                <div class="col-sm-12 all">
+                <h2 id="players_num">Players: ';
+                if($online){
+                    $player_num = $status -> players -> online;
+                    echo $player_num .'/'. $status -> players -> max;
+                }else {
+                    echo 'none';
+                }
+                echo '</h2>
+                    <div class="list">
+                            ';
+                            if($online) {
+                                $player_num = $status -> players -> online;
+                                if($player_num > 0) {
+                                    echo '<table class="table table-bordered table-dark table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">Head</th>
+                                                    <th scope="col">Players</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            ';
+                                            foreach ($status->players->list as $player) {
+                                                echo '<tr>
+                                                            <th scope="row"><img src="https://minotar.net/avatar/' . $player . '/50"</th>
+                                                            <td>' . $player . '
+                                                        </tr>
+                                                ';
+                                            }
+                                            echo'</tbody>';
+                                        ;
+                                }
+                            }
+                            echo'
+                    <div/>
+                </div>
+            </div>
+
+            ';
+        
+    ?>
 </body>
 </html>
